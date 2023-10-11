e50a0c9b843c987cc499d2169b2d429549e0c6cc
Haroon <haroonr1@umbc.edu>
2023-10-11 18:18:50 -0400

    fixed flask server

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Blueprint, request, jsonify
 
-app = Flask(__name)
 
-@app.route('/api/submit', methods=['POST'])
+api = Blueprint('api', __name__)
+
+@api.route('/api/submit', methods=['POST'])
 def submit_data():
     data = request.json
     # Process the data as needed (e.g., save it to a database)
