88ff8783577cc6c60ef92f741c470857b7bb7ed4
nspq <nblsyed@hotmail.com>
2023-10-15 12:13:42 -0400

    Setting up basic homepage

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-cols text-center">
+        <div className="w-1/8 bg-slate-200 h-full p-4 hidden md:flex flex-col">
+          <div className="px-4">
+            <p>Project Chainmail</p>
+          </div>
+          <div className="justify-stretch py-5 grid grid-cols-1 gap-3 my-auto">
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Step 1
+            </div>
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Step 2
+            </div>
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Results
+            </div>
+          </div>
+          <div>
+            FOOTER
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-3 w-1/2 mx-auto h-1/2">
+          <div className="text-4xl pt-10" >Welcome</div>
+          <div className="block">
+          <button className="bg-slate-100 shadow-md px-4 py-3"> Get started</button></div>
+        </div>
+     </div>
+  );  
+}
+
+export default App;
