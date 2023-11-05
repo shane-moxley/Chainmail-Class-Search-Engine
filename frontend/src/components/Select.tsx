dc8f5898cfac0dbc6053a42d5097f4b3f778b673
Haroon <haroonr1@umbc.edu>
2023-11-05 11:55:46 -0500

    add more API call logic to Select page (commented out)

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -1,20 +1,99 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Container, ScrollArea, Checkbox, Button, Card, Box,  } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const Select = () => {
-
   const navigate = useNavigate();
-  const routeChange = () =>{ 
-    let path = `select`; 
+  // const location = useLocation();
+  // const queryParams = new URLSearchParams(location.search);
+
+  // const [selectedCourses, setSelectedCourses] = useState<string[]>([]);
+  // const [coursesData, setCoursesData] = useState<any>(null); // Initialize as null
+
+  // useEffect(() => {
+  //   // Extract the "Major" and "Year" parameters from the URL
+  //   const major = queryParams.get('Major');
+  //   const year = queryParams.get('Year');
+
+  //   if (major && year) {
+  //     // Make an API call to get courses based on the Major and Year
+  //     axios
+  //       .get('/getCourseList', {
+  //         params: {
+  //           Major: major,
+  //           Year: year,
+  //         },
+  //       })
+  //       .then((response) => {
+  //         // Handle the API response here
+  //         const courses = response.data; // Assuming the API returns the data as described
+  //         setCoursesData(courses);
+  //       })
+  //       .catch((error) => {
+  //         // Handle API call errors here
+  //         console.error('API call failed:', error);
+  //         console.error('Response status:', error.response.status);
+
+  //       });
+  //   }
+  // }, [queryParams]);
+
+
+  const routeChange = () => {
+    // // Make an API call to send the selected courses to the backend
+    // axios
+    //   .post('/sendStudentData', {
+    //     selectedCourses,
+    //   })
+    //   .then((response) => {
+    //     // Handle the response from the backend, if needed
+    //     console.log('Student data sent successfully:', response.data);
+    //     // navigate to the next page 
+    //     let path = `/search`; 
+    //     navigate(path);
+    //   })
+    //   .catch((error) => {
+    //     // Handle API call errors here
+    //     console.error('Failed to send student data:', error);
+    //   });
+    // navigate to the next page 
+    let path = `/search`; 
     navigate(path);
-  }
+  };
+
+  // const handleCheckboxChange = (course: string, checked: boolean) => {
+  //   if (checked) {
+  //     setSelectedCourses((prevCourses) => [...prevCourses, course]);
+  //   } else {
+  //     setSelectedCourses((prevCourses) => prevCourses.filter((c) => c !== course));
+  //   }
+  // };
 
   return (
     <Container className='flex flex-col gap-y-6 justify-center'>
       <h1>Let's see how far you already got...</h1>
       <Container className='flex flex-col md:flex-row md:gap-x-4 space-y-4 items-center'>
 
+        {/* uncomment when APIs are implemented */}
+        {/* {coursesData &&
+          Object.keys(coursesData).map((category) => (
+            <Card key={category} w={250} h={600}>
+              <h4>{category}</h4>
+              <Box className='space-y-4'>
+                {coursesData[category].map((course: string) => (
+                  <Checkbox
+                    key={course}
+                    value={course}
+                    label={course}
+                    checked={selectedCourses.includes(course)}
+                    onChange={(event) => handleCheckboxChange(course, event.target.checked)}
+                  />
+                ))}
+              </Box>
+            </Card>
+          ))} */}
+     
           
           <Card  w={250} h={600} mt={16}>
               <h4>Select Math Classes:</h4>
--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Select, Button, MantineProvider, Container } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios'; // Import Axios library for making HTTP requests
 
 const Welcome = () => {
   const navigate = useNavigate();
@@ -10,25 +9,8 @@ const Welcome = () => {
 
   const routeChange = () => {
     if (selectedMajor && selectedYear) {
-      // // Make an API call to "/getCourseList" with selectedMajor and selectedYear
-      // axios
-      //   .get('/getCourseList', {
-      //     params: {
-      //       Major: selectedMajor,
-      //       Year: selectedYear,
-      //     },
-      //   })
-      //   .then((response) => {
-      //     // Handle the API response here, e.g., navigate to the next page
-      //     // or process the data returned from the API
-      //     navigate('/select'); // Navigate to the next page
-      //   })
-      //   .catch((error) => {
-      //     // Handle API call errors here
-      //     console.error('API call failed:', error);
-      //   });
-
-      navigate('/select'); // Navigate to the next page (remove when api implemented)
+      // Navigate to the "/select" page and pass the selectedMajor and selectedYear as URL parameters
+      navigate(`/select?Major=${selectedMajor}&Year=${selectedYear}`);
     }
   };
 
@@ -36,7 +18,7 @@ const Welcome = () => {
     <Container className='space-y-8 grow flex flex-col justify-center'>
       <h1>Welcome, Student</h1>
 
-      <p> We introduce to you the UMBC Chainmail College Assistant! (Your introductory text here)</p>
+      <p> We introduce to you the UMBC Chainmail College Assistant! </p>
 
       <Select
         label="Select your Major"
